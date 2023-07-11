c659a5004577453296bf348eca3d0bcf6916ea04
Bruno Lopes <brunomantovanlopes@gmail.com>
2023-07-11 14:36:30 -0300

    Fix: Roles can now be altered without bugs and can be made by batch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter } from 'react-router-dom';
 
