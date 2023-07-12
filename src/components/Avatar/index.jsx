c81e0e90865bb417f4e7c72f4fa5a0f13ce46edf
EduardoRag <eduardojarek66@gmail.com>
2023-07-12 14:37:24 -0300

    Fix: Fixed avatar bug

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -5,28 +5,24 @@ import jwtDecode from "jwt-decode";
 
 export default function LetterAvatars({ handleOpen }) {
   const name = jwtDecode(localStorage.getItem("token")).name.split(" ");
+  let letterAvatar = '';
 
   if (name.length === 1) {
-    const letterAvatar = name[0].charAt(0);
+    letterAvatar = name[0].charAt(0);
   }
 
   if (name.length === 2) {
     const firstName = name[0];
     const lastName = name[1];
 
-    const letterAvatar = firstName.charAt(0) + lastName.charAt(0);
+    letterAvatar = firstName.charAt(0) + lastName.charAt(0);
   }
-
-  const firstName = name[0];
-  const lastName = name[1];
-
-  const letterAvatar = firstName.charAt(0) + lastName.charAt(0);
 
   return (
     <Stack direction="row" spacing={2}>
       <Avatar
         sx={{ bgcolor: deepPurple["500"], marginRight: "0.9rem" }}
-        // onClick={() => handleOpen()}
+      // onClick={() => handleOpen()}
       >
         {letterAvatar}
       </Avatar>
