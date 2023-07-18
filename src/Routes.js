681143d85733500a9d83d9c3399a02b2ba10e4a2
Bruno Novais <novais-bruno@hotmail.com>
2023-07-17 23:26:36 -0300

    Featch: Layot page login and page forget password

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import News from './pages/News.jsx';
 import Bills from './pages/Bills.jsx';
 import CommonAreas from './pages/CommonAreas.jsx';
-import Test from './pages/Test.jsx';
-import RulesOfProcedure from './pages/RulesOfProcedure.jsx';
+import ForgetPassword from './pages/ForgetPassword.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import News from './pages/News.jsx';
+import Register from './pages/Register.jsx';
 import RoleDistribution from './pages/RoleDistribution.jsx';
+import RulesOfProcedure from './pages/RulesOfProcedure.jsx';
+import Test from './pages/Test.jsx';
 
 // function ProtectedRoutes({ redirectTo }) {
 //   const token = localStorage.getItem('token');
@@ -19,6 +20,7 @@ export default function MainRoutes() {
     <Routes>
       <Route path='/' element={<Login />} />
       <Route path='/registro' element={<Register />} />
+      <Route path='/esqueceusenha' element={<ForgetPassword />} />
       <Route path='/home' element={<Home />} />
       <Route path='/noticias' element={<News />} />
       <Route path='/contas' element={<Bills />} />
