c81b27dd54d41d95b4465fe9f08e62dbef4cebe0
Bruno Lopes <brunomantovanlopes@gmail.com>
2023-07-19 15:17:17 -0300

    FEAT: INsertion of all routes for news and modal to edit, create and a button to delete

---
+++ src/components/CreateNewsModal/CreateNewsModal.jsx
@@ -0,0 +1,104 @@
+import jwtDecode from "jwt-decode";
+import React, { useState, useEffect } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+
+const CreateNewsModal = ({ show, handleClose, handleCreate, formData }) => {
+  const { id } = jwtDecode(localStorage.getItem("token"));
+
+  const [formValues, setFormValues] = useState({
+    ULRimage: "",
+    alt: "",
+    title: "",
+    content: "",
+    user: id
+  });
+
+  useEffect(() => {
+    if (formData) {
+      setFormValues(formData);
+    }
+  }, [formData]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleClearForm = () => {
+    setFormValues({
+      ULRimage: "",
+      alt: "",
+      title: "",
+      content: "",
+      user: id
+    });
+  };
+
+  const handleSubmit = () => {
+    handleCreate(formValues);
+    handleClearForm();
+    handleClose();
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Criar nova noticia</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="image">
+            <Form.Label>URL da imagem</Form.Label>
+            <Form.Control
+              type="text"
+              name="ULRimage"
+              value={formValues.ULRimage}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="alt">
+            <Form.Label>Texto para leitura de tela da Imagem</Form.Label>
+            <Form.Control
+              type="text"
+              name="alt"
+              value={formValues.alt}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="title">
+            <Form.Label>Titulo</Form.Label>
+            <Form.Control
+              type="text"
+              name="title"
+              value={formValues.title}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="content">
+            <Form.Label>Conteudo</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              name="content"
+              value={formValues.content}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Fechar
+        </Button>
+        <Button variant="primary" onClick={handleSubmit}>
+          Salvar Alterações
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CreateNewsModal;
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,9 +3,12 @@ import { useEffect, useState } from "react";
 import Header from "../components/Header/index.jsx";
 import api from "../services/api";
 import "../styles/news.css";
+import CreateNewsModal from "../components/CreateNewsModal/CreateNewsModal.jsx";
 
-function News() {
+export default function News() {
   const [news, setNews] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [editNewsData, setEditNewsData] = useState(null);
 
   async function getNews() {
     try {
@@ -41,18 +44,64 @@ function News() {
 
   const { role } = jwtDecode(localStorage.getItem("token"));
 
+  async function deleteNews(id) {
+    try {
+      await api.delete(`news/deletar/${id}`);
+      getNews();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Função para abrir o modal
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  // Função para fechar o modal
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  // Função para criar a notícia
+  const handleCreateNews = async (formData) => {
+    try {
+      await api.post("/news/criar", formData);
+
+      handleCloseModal();
+
+      getNews();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleOpenEditModal = (news) => {
+    setEditNewsData(news);
+    setShowModal(true);
+  };
+
   return (
     <div>
       <Header condominiumName={"Residencial Canadá"} />
       {role === "admin" ? (
         <div>
-          <button className="newsButton">
-            <a href="/news/create">Criar notícia</a>
+          <button className="newsButton" onClick={handleOpenModal}>
+            Criar notícia
           </button>
         </div>
       ) : (
         ""
       )}
+
+      {/* Modal */}
+      <CreateNewsModal
+        show={showModal}
+        handleClose={handleCloseModal}
+        handleCreate={handleCreateNews}
+        formData={editNewsData}
+      />
+
       <section className="newsContent">
         <h1>Notícias</h1>
         <div className="newsCards">
@@ -64,14 +113,29 @@ function News() {
                 <p>{news.content.substring(0, 200).concat("...")}</p>
                 <p>
                   {role === "admin" ? (
-                    <button className="deleteBtn">
-                      <a href={`/news/delete/${news.id}`}>Deletar</a>
+                    <button
+                      onClick={() => deleteNews(news._id)}
+                      type="button"
+                      className="deleteBtn"
+                    >
+                      Deletar
                     </button>
                   ) : (
                     ""
                   )}
                 </p>
-                <p>{role === "admin" ? "Alterar" : ""}</p>
+                <p>
+                  {role === "admin" ? (
+                    <button
+                      className="btnEditNews"
+                      onClick={() => handleOpenEditModal(news)}
+                    >
+                      Editar
+                    </button>
+                  ) : (
+                    ""
+                  )}
+                </p>
               </div>
             </div>
           ))}
@@ -80,5 +144,3 @@ function News() {
     </div>
   );
 }
-
-export default News;
